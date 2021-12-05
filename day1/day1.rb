9a2427c12fa029d59fe2d1f4b67e01b49df45750
Philibert Dugas <philibert.dugas@gmail.com>
2021-12-05 08:20:03 -0500

    Rubocop pass

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,23 +1,23 @@
 require 'pry'
 
 def part_one
-    values = File.readlines("part1.txt")
-    count_increases(values.map(&:to_i))
+  values = File.readlines('part1.txt')
+  count_increases(values.map(&:to_i))
 end
 
 def part_two
-    values = File.readlines("part2.txt")
-    window_sum = []
-    values.each_cons(3) { |window| window_sum << window.map(&:to_i).sum }
-    count_increases(window_sum)
+  values = File.readlines('part2.txt')
+  window_sum = []
+  values.each_cons(3) { |window| window_sum << window.map(&:to_i).sum }
+  count_increases(window_sum)
 end
 
 def count_increases(values)
-    counter = 0
-    values.each_cons(2) do |first, second|
-        counter +=1 if second > first
-    end
-    counter
+  counter = 0
+  values.each_cons(2) do |first, second|
+    counter += 1 if second > first
+  end
+  counter
 end
 
 puts part_two
--- day2/day2.rb
+++ day2/day2.rb
@@ -1,83 +1,82 @@
 require 'pry'
 
 class Submarine
-    def initialize(depth, horizontal_position)
-        @depth = depth
-        @horizontal_position = horizontal_position
-    end
+  def initialize(depth, horizontal_position)
+    @depth = depth
+    @horizontal_position = horizontal_position
+  end
 
-    def forward(units)
-        @horizontal_position += units
-    end
+  def forward(units)
+    @horizontal_position += units
+  end
 
-    def down(units)
-        @depth += units
-    end
+  def down(units)
+    @depth += units
+  end
 
-    def up(units)
-        @depth -= units
-    end
+  def up(units)
+    @depth -= units
+  end
 
-    def multiplied_position
-        @depth * @horizontal_position
-    end
+  def multiplied_position
+    @depth * @horizontal_position
+  end
 end
 
 class ComplicatedSubmarine
-    def initialize(depth, horizontal_position, aim)
-        @depth = depth
-        @horizontal_position = horizontal_position
-        @aim = aim
-    end
+  def initialize(depth, horizontal_position, aim)
+    @depth = depth
+    @horizontal_position = horizontal_position
+    @aim = aim
+  end
 
-    def forward(units)
-        @horizontal_position += units
-        @depth += @aim * units
-    end
+  def forward(units)
+    @horizontal_position += units
+    @depth += @aim * units
+  end
 
-    def down(units)
-        @aim += units
-    end
+  def down(units)
+    @aim += units
+  end
 
-    def up(units)
-        @aim -= units
-    end
+  def up(units)
+    @aim -= units
+  end
 
-    def multiplied_position
-        @depth * @horizontal_position
-    end
+  def multiplied_position
+    @depth * @horizontal_position
+  end
 end
 
 def part_one
-    submarine = Submarine.new(0, 0)
-    commands = File.readlines("part1.txt")
-    execute_commands(commands, submarine)
+  submarine = Submarine.new(0, 0)
+  commands = File.readlines('part1.txt')
+  execute_commands(commands, submarine)
 
-    puts submarine.multiplied_position
+  puts submarine.multiplied_position
 end
 
 def part_two
-    submarine = ComplicatedSubmarine.new(0, 0, 0)
-    commands = File.readlines("part2.txt")
-    execute_commands(commands, submarine)
+  submarine = ComplicatedSubmarine.new(0, 0, 0)
+  commands = File.readlines('part2.txt')
+  execute_commands(commands, submarine)
 
-    puts submarine.multiplied_position
+  puts submarine.multiplied_position
 end
 
 def execute_commands(commands, submarine)
-    commands.each do |command|
-        instruction, units = command.split(" ")
-        units = units.to_i
-        case instruction
-        when "forward"
-            submarine.forward(units)
-        when "down"
-            submarine.down(units)
-        when "up"
-            submarine.up(units)
-        else
-        end
+  commands.each do |command|
+    instruction, units = command.split(' ')
+    units = units.to_i
+    case instruction
+    when 'forward'
+      submarine.forward(units)
+    when 'down'
+      submarine.down(units)
+    when 'up'
+      submarine.up(units)
     end
+  end
 end
 
 puts part_one
